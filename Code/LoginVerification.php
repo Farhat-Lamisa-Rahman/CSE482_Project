8199b66e15bdb8745517703e3ae3fbeba2bd15b5
Farhat-Lamisa-Rahman <lamisa.farhat@northsouth.edu>
2021-05-11 15:52:37 +0600

    Login page added

---
+++ Code/LoginVerification.php
@@ -0,0 +1,25 @@
+<?php include('db_connect.php'); ?>
+
+<?php
+
+$Password=$_POST['Password'];
+$Email=$_POST['email'];
+  $sql = "SELECT `Password`, `Email` FROM `customerdetails` WHERE Email LIKE '%$Email%'";
+  $result = $connection->query($sql);
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) 
+    {
+      
+      if($Email==$row["Email"])
+      {
+        header("Location: loginpage.php");
+        
+      }
+      else echo "<br>"." Try again !!!  " ;
+    }
+  } 
+
+
+$connection->close();
+?>
